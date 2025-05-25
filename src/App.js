82fc8b154b8f3c1a334750cde8ea94b10ec85942
zoeltea <zoeltea@gmail.com>
2025-05-25 16:19:32 +0700

    init profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Zul Yatman</h1>
+        <nav>
+          <a href="#about">About Me</a>
+          <a href="#skills">skills</a>
+          <a href="#contact">contact</a>
+        </nav>
       </header>
+
+      <section id="about">
+        <h2>About Me</h2>
+        <p>Dedicated as Quality Engineer with 9 years of experience, now moving into Software Engineering to grow my technical skills. I’ve built tools to automate test cases and generate reports, making my team more efficient. I’ve also improved test automation speed, fixed unreliable tests (for Android, iOS, and web), and trained engineers on automation frameworks. My skills include API testing, CI/CD (Jenkins, GitLab), and managing testing for complex payment and live streaming systems.</p>
+      </section>
+
+      <section id="skills">
+        <h2>Keterampilan</h2>
+        <ul>
+          <li>Quality Engineer</li>
+          <li>Software Engineer</li>
+          <li>Golan, Appium</li>
+        </ul>
+      </section>
+
+      <section id="contact">
+        <h2>Kontak</h2>
+        <p>Email: zoeltea@gmail.com</p>
+      </section>
+
+      <footer>
+        <p>© 2025 Zul Yatman</p>
+      </footer>
     </div>
   );
 }
